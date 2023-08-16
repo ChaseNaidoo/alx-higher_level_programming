0f44d37ec8183cb286809707a450c4b8be19561c
root <chasenaidoo9@gmail.com>
2023-08-16 09:27:47 -0700

    Multiply new dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
 
     new_dictionary = {}
     for i in a_dictionary:
-        new_dictionary = a_dictionary[i] * 2
+        new_dictionary[i] = a_dictionary[i] * 2
     return a_dictionary
