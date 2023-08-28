fd6f7ac3a2198732a864868cbeaa55271385fa38
root <chasenaidoo9@gmail.com>
2023-08-28 06:26:41 -0700

    Fixed pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
-
-    except:
+    except error:
         pass
     print()
     return count
-
