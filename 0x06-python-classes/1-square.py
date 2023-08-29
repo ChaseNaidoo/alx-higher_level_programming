c6265505ab73de95b27d8e3b674774695f434c5a
root <chasenaidoo9@gmail.com>
2023-08-29 07:49:00 -0700

    Private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size):
+        """Instantiation with size.
+
+        Args:
+        size (int): Size of square.
+        """
+        self.__size = size
