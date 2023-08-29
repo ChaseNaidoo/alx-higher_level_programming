cb6cca16934fbc3c5f6bb857ce89bab84414b3b8
root <chasenaidoo9@gmail.com>
2023-08-29 08:17:05 -0700

    Class with error messages

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0):
+        """Instantiation with size.
+
+        Args:
+        size (int): Size of square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
