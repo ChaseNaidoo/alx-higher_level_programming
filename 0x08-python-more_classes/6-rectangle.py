586a3b909de6d88f84d08c2adba43afaf5cea66e
root <chasenaidoo9@gmail.com>
2023-09-04 09:15:21 -0700

    Print instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,8 @@
 class Rectangle:
     """Class that defines a rectangle"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """method that initializes the instance
 
