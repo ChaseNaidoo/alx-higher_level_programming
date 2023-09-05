48409a6f5fc3a7e11798d46a11765dbd48d437ed
root <chasenaidoo9@gmail.com>
2023-09-05 09:19:37 -0700

    Prevent new instance

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """This is a module that containts a clas that avoids
-dynmaically created attributes"""
-
+dynmaically created attributes
+"""
 
 class LockedClass:
-    __slots__ = ['first_name']
-
-    def __init__(self):
-        """Init method"""
-        pass
+    """Prevents new instance for attribute"""
+    __slots__ = ["first_name"]
